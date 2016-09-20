033057e6c9b8666a8d704541b38ebb60ad7ef357
diananr <navarroruiz.diana>
2016-09-20 11:18:03 -0500

    validacion de boton y contador de caracteres

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
 window.addEventListener("load", function(){
 	
-	var boton = document.getElementById("btn");
-	boton.addEventListener("click", function(){
+	var enviarTweet = document.getElementById("btn");
+	var cajaTexto = document.getElementById("msj");
+	var conta = document.getElementById("contador");
 
-		var texto = document.getElementById("msj").value;
-		var cuadroGrande = document.getElementById("contenedor");
-
-		var hermano = document.createElement("div");
-		hermano.innerText = texto;
-
-		if(!cuadroGrande.childNodes[0]){
-			cuadroGrande.appendChild(hermano);
-		}
-		else{
-			cuadroGrande.insertBefore(hermano, cuadroGrande.childNodes[0]);
-		}
-
-		hermano.classList.add("tweets");
+	enviarTweet.addEventListener("click", function(e){
+		e.preventDefault();
+		agregarTweet(cajaTexto.value);
+		cajaTexto.value = "";
+		conta.textContent = "140";
+		enviarTweet.disabled = true;
 	});
+
+	cajaTexto.addEventListener("keyup", function(){
+			deshabilitarBoton(cajaTexto);
+			contadorCaracteres(cajaTexto);
+
+	});
+
+	function agregarTweet(texto){
+		var newTweet = document.createElement("div");
+		newTweet.innerText = texto;
+
+		var contenedorTwets = document.getElementById("contenedor");
+		contenedorTwets.insertBefore(newTweet, contenedorTwets.childNodes[0]);
+
+		newTweet.classList.add("tweets");
+	}
+	function deshabilitarBoton(cText){
+		if(cText.value.length > 0){
+			enviarTweet.disabled = false;
+		}
+		else if(cText.value.length > 139 || cText.value.length == 0){
+			enviarTweet.disabled = true;
+		}
+	}
+	function contadorCaracteres(texto){
+		var caracteres = texto.value.length;
+		conta.innerText = 140 - caracteres;
+	}
 });
